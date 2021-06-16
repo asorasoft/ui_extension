12e9ecc399cd8c90d3d6912682813ff26057ad37
Sor Thyrith <sor.thyrith@gmail.com>
2021-06-16 17:13:11 +0700

    Text Theme from context

--- lib/extensions/build_context.dart
+++ lib/extensions/build_context.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../utils/_utils.dart';
 
 extension XBuildContext on BuildContext {
   Future<T?> push<T extends Object?>(Widget nextPage, {bool replace = false}) {
@@ -27,4 +26,28 @@ extension XBuildContext on BuildContext {
       currentFocus.unfocus();
     }
   }
+
+  TextStyle? get headline1 => Theme.of(this).textTheme.headline1;
+
+  TextStyle? get headline2 => Theme.of(this).textTheme.headline2;
+
+  TextStyle? get headline3 => Theme.of(this).textTheme.headline3;
+
+  TextStyle? get headline4 => Theme.of(this).textTheme.headline4;
+
+  TextStyle? get headline5 => Theme.of(this).textTheme.headline5;
+
+  TextStyle? get headline6 => Theme.of(this).textTheme.headline6;
+
+  TextStyle? get bodyText1 => Theme.of(this).textTheme.bodyText1;
+
+  TextStyle? get bodyText2 => Theme.of(this).textTheme.bodyText2;
+
+  TextStyle? get captionStyle => Theme.of(this).textTheme.caption;
+
+  TextStyle? get buttonStyle => Theme.of(this).textTheme.button;
+
+  TextStyle? get subtitle1 => Theme.of(this).textTheme.subtitle1;
+
+  TextStyle? get subtitle2 => Theme.of(this).textTheme.subtitle2;
 }
