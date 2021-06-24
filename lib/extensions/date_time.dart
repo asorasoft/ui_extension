ce154e2d89febe9ece5a1eadf404ebfbf3966977
Sor Thyrith <sor.thyrith@gmail.com>
2021-06-24 15:01:54 +0700

    Format date time

--- lib/extensions/date_time.dart
+++ lib/extensions/date_time.dart
@@ -1,3 +1,11 @@
-extension XDateTime on DateTime {
+import 'package:intl/intl.dart';
 
+extension XDateTime on DateTime {
+  String formatDateTime([String? locale]) {
+    return DateFormat.yMMMMEEEEd(locale).format(this) + ' ' + DateFormat.jm(locale).format(this);
+  }
+
+  String formatDate([String? locale]) {
+    return DateFormat.yMMMMEEEEd(locale).format(this);
+  }
 }
